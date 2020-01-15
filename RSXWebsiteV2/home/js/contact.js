c5719df4c3194f160f64ec04864c65569f590599
Nicholas Jones <34221038+nj31415@users.noreply.github.com>
2020-01-15 13:37:43 -0500

    Add files via upload

---
+++ RSXWebsiteV2/home/js/contact.js
@@ -0,0 +1,15 @@
+function openEmail() {
+	window.location.href = "mailto:virginiatechrocksatx@gmail.com";
+}
+
+function openFacebook() {
+	window.location.href = "https://www.facebook.com/VT-Rocksat-X-102463464507404/";
+}
+
+function openTwitter() {
+	window.location.href = "https://twitter.com/vtrocksatx";
+}
+
+function openInstagram() {
+	window.location.href = "https://www.instagram.com/vtrocksatx/";
+}
